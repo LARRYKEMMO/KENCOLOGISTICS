6476c45b64de21dafdc10763c2f2de3befad7f72
LARRYKEMMO <larryloickemmotejiokem@gmail.com>
2024-02-27 13:46:48 -0400

    Commit Reminders

--- KENCO LOGISTIQUES APP/KENCO LOGISTIQUES APP/Reminder.Designer.cs
+++ KENCO LOGISTIQUES APP/KENCO LOGISTIQUES APP/Reminder.Designer.cs
@@ -1499,7 +1499,6 @@
             this.Column7});
             this.dataGridView1.Location = new System.Drawing.Point(29, 399);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
             this.dataGridView1.Size = new System.Drawing.Size(1401, 168);
@@ -1510,7 +1509,6 @@
             this.Column1.HeaderText = "Vehicle Plate Number";
             this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
             this.Column1.Width = 200;
             // 
             // Column2
@@ -1518,7 +1516,6 @@
             this.Column2.HeaderText = "Insurance Expiry Date";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
             this.Column2.Width = 200;
             // 
             // Column3
@@ -1526,7 +1523,6 @@
             this.Column3.HeaderText = "Reminder";
             this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
             this.Column3.Width = 200;
             // 
             // Column4
@@ -1534,7 +1530,6 @@
             this.Column4.HeaderText = "Technical Visit Expiry Date";
             this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
-            this.Column4.ReadOnly = true;
             this.Column4.Width = 200;
             // 
             // Column5
@@ -1542,7 +1537,6 @@
             this.Column5.HeaderText = "Reminder";
             this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
-            this.Column5.ReadOnly = true;
             this.Column5.Width = 200;
             // 
             // Column6
@@ -1550,7 +1544,6 @@
             this.Column6.HeaderText = "Registration Card Expiry Date";
             this.Column6.MinimumWidth = 6;
             this.Column6.Name = "Column6";
-            this.Column6.ReadOnly = true;
             this.Column6.Width = 200;
             // 
             // Column7
@@ -1558,7 +1551,6 @@
             this.Column7.HeaderText = "Reminder";
             this.Column7.MinimumWidth = 6;
             this.Column7.Name = "Column7";
-            this.Column7.ReadOnly = true;
             this.Column7.Width = 200;
             // 
             // SaveButton
